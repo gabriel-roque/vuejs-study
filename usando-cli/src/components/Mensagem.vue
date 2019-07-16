e30bc686384f09faa7c32f9b3063e169508a0e2a
gabriel-roque <gabrielrms.dll@gmail.com>
2019-07-16 09:34:19 -0300

    Projeto com CLI

---
+++ usando-cli/src/components/Mensagem.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="index">
+        <div class="center">
+            <h1>{{msg}}</h1>
+        </div>
+        <div class="center">
+            <p> {{nome}} </p>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: 'index',
+    data() {
+        return {
+            msg: 'Ola mundo!'
+        }
+    },
+    props: {
+        nome: String
+    }
+}
+
+</script>
+
+<style>
+    .center {
+        display: flex;
+        justify-content: center;
+    }
+</style>
---
+++ usando-cli/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Mensagem nome="Gabriel Roque"></Mensagem>
+  </div>
+</template>
+
+<script>
+import Mensagem from '../components/Mensagem.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Mensagem
+  }
+}
+</script>
