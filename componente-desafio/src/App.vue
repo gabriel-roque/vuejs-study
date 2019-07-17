386ddcf16d2006e5844cb2fd641666d8ef125c12
gabriel-roque <gabrielrms.dll@gmail.com>
2019-07-17 09:57:52 -0300

    Organizando Components

---
+++ componente-desafio/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-app>
+        <Menu/>
+        <Toolbar/>
+        <v-content>
+            <Slider/>
+        </v-content>
+        <Footer/>
+    </v-app>
+</template>
+
+<script>
+    import Menu from './components/templates/Menu.vue'
+    import Toolbar from './components/home/Toolbar.vue'
+    import Slider from './components/home/Slider.vue'
+    import Footer from './components/templates/Footer.vue'
+
+    export default {
+        components: {
+            Menu,
+            Toolbar,
+            Slider,
+            Footer,
+        }
+    }
+</script>
---
+++ componente-desafio/src/components/home/Toolbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-toolbar app>
+        <v-toolbar-title class="headline text-uppercase">
+            <span>Desafio</span>
+            <span class="font-weight-light">Componente</span>
+        </v-toolbar-title>
+    </v-toolbar>
+</template>
+
+<script>
+    export default {
+        name: "Toolbar"
+    }
+</script>
+
+<style scoped>
+
+</style>
